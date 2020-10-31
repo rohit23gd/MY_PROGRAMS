b0f9ad37dc31c65a267d13947cf8c4c599bf14f9
rohit23gd <70327079+rohit23gd@users.noreply.github.com>
2020-10-31 22:01:32 +0530

    Program to calculate "Greatest Common Divisor"
    Fastest way to calculate Greatest Common divisor using #Euclidean Algorithm.

---
+++ gcd_Euclidean algorithm.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int gcd_fast(int a, int b) {
+  int current_gcd = 1,temp,l=b,s=a;
+  if(a>=b){
+     l = a;
+     s =b;
+  }
+
+  if(s==1)  // 1 is always a GCD of other number where(l>b)
+    return 1;
+  while(s>0)
+  {
+    temp =s;   //Euclidean algorithm.
+    s= l%s;
+    l=temp;
+  }
+
+  return l;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
+}
